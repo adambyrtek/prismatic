8d737c9bbf974436991c653f900609804302d53a
Adam Byrtek <adambyrtek@gmail.com>
2015-06-11 17:22:19 +0100

    Metadata for distutils

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'jsonmirror',
+  packages = ['jsonmirror'],
+  version = '0.1',
+  description = 'Serialization between JSON dicts and data objects designed for REST APIs',
+  author = 'Adam Byrtek',
+  author_email = 'adambyrtek@gmail.com',
+  url = 'https://github.com/adambyrtek/jsonmirror',
+  download_url = 'https://github.com/adambyrtek/jsonmirror/tarball/0.1',
+  keywords = ['json', 'orm', 'serialization', 'api', 'rest'],
+  classifiers = [],
+)
+
