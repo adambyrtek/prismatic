9ca9243ee74932a605af37dfa281287f67f0b7b6
Adam Byrtek <adambyrtek@gmail.com>
2015-06-17 15:17:31 +0100

    Improve packaging and upload wheels

--- Makefile
+++ Makefile
@@ -10,3 +10,4 @@ pypi-register:
 
 pypi-upload:
 	python setup.py sdist upload -r pypi
+	python setup.py bdist_wheel upload -r pypi
--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+def read(*paths):
+    with open(os.path.join(*paths), 'r') as f:
+        return f.read()
 
 setup(
   name = 'prismatic',
   packages = ['prismatic'],
-  version = '0.2',
+  version = '0.3',
   description = 'Serialization between JSON dicts and data objects designed for REST APIs',
+  long_description=read('README.rst'),
+  license='MIT',
   author = 'Adam Byrtek',
   author_email = 'adambyrtek@gmail.com',
   url = 'https://github.com/adambyrtek/prismatic',
